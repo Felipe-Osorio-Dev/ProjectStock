483b04e034353f002d6e22565b9e8cdb0110cb6f
Felipe Osorio <felipe.osorio.dev@gmail.com>
2026-07-19 14:48:22 -0300

    feat: Criação e Implementação da classe de Configuração DbContext.

---
+++ StockAPI/AppData/AppDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StockAPI.Models;
+
+namespace StockAPI.AppData
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions options) : base(options) {}
+
+        public DbSet<ProductModel> Produtos { get; set; }
+    }
+}
