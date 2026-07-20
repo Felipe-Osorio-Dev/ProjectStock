5f9badfaaf8db37fc884530018afbd6c9c079479
Felipe Osorio <felipe.osorio.dev@gmail.com>
2026-07-20 04:34:34 -0300

    feat: Criação e Implementação do Repository, classe responsavel por lidar com o banco de dados.

---
+++ StockAPI/Repositorys/IProductRepository.cs
@@ -0,0 +1,12 @@
+﻿using StockAPI.Models;
+
+namespace StockAPI.Repositorys
+{
+    public interface IProductRepository
+    {
+        Task<IEnumerable<ProductModel>> GetAllProductsAsync();
+        Task<ProductModel?> GetProductByIdAsync(long id);
+        Task<ProductModel?> GetProductByEANAsync(string EAN);
+        Task RegisterProductAsync(ProductModel product);
+    }
+}
---
+++ StockAPI/Repositorys/ProductRepository.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.EntityFrameworkCore;
+using StockAPI.AppData;
+using StockAPI.Models;
+
+namespace StockAPI.Repositorys
+{
+    public class ProductRepository : IProductRepository
+    {
+        private readonly AppDbContext _dbContext;
+
+        public ProductRepository(AppDbContext dbContext)
+        {
+            _dbContext = dbContext;
+        }
+
+        public async Task<IEnumerable<ProductModel>> GetAllProductsAsync()
+        {
+            return await _dbContext.Produtos.ToListAsync();
+        }
+
+        public async Task<ProductModel?> GetProductByEANAsync(string EAN)
+        { 
+            return await _dbContext.Produtos.FirstOrDefaultAsync(p => p.EAN == EAN);
+        }
+
+        public async Task<ProductModel?> GetProductByIdAsync(long id)
+        {
+            return await _dbContext.Produtos.FindAsync(id);
+        }
+
+        public async Task RegisterProductAsync(ProductModel product)
+        {
+            await _dbContext.Produtos.AddAsync(product);
+            await _dbContext.SaveChangesAsync();
+        }
+    }
+}
