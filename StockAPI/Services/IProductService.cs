94f1f1fb02e2b73583d4d8e9fff7731eb785dd94
Felipe Osorio <felipe.osorio.dev@gmail.com>
2026-07-21 04:38:38 -0300

    feat: Criação e implementação da classe de Service para lidar com a logica de requests e responses vinda do client.

---
+++ StockAPI/Services/IProductService.cs
@@ -0,0 +1,12 @@
+﻿using StockAPI.Dto;
+
+namespace StockAPI.Services
+{
+    public interface IProductService
+    {
+        Task<IEnumerable<ProductDTO>> GetAllProductsAsync();
+        Task<ProductDTO?> GetProductByIdAsync(long id);
+        Task<ProductDTO?> GetProductByEANAsync(string EAN);
+        Task RegisterProductAsync(ProductDTO product);
+    }
+}
---
+++ StockAPI/Services/ProductService.cs
@@ -0,0 +1,55 @@
+﻿using MapsterMapper;
+using StockAPI.Dto;
+using StockAPI.Models;
+using StockAPI.Repositorys;
+
+namespace StockAPI.Services
+{
+    public class ProductService : IProductService
+    {
+        private readonly IProductRepository _repository;
+        private readonly IMapper _mapper;
+
+        public ProductService(IProductRepository repository, IMapper mapper)
+        {
+            _repository = repository;
+            _mapper = mapper;
+        }
+
+        public async Task<IEnumerable<ProductDTO>> GetAllProductsAsync()
+        {
+            return _mapper.Map<List<ProductDTO>>(_repository.GetAllProductsAsync());
+        }
+
+        public async Task<ProductDTO?> GetProductByEANAsync(string EAN)
+        {
+            var product = _repository.GetProductByEANAsync(EAN);
+
+            if (product == null)
+            {
+                return null;
+            }
+
+            return _mapper.Map<ProductDTO>(product);
+        }
+
+        public async Task<ProductDTO?> GetProductByIdAsync(long id)
+        {
+            var product = _repository.GetProductByIdAsync(id);
+
+            if (product == null)
+            {
+                return null;
+            }
+
+            return _mapper.Map<ProductDTO>(product);
+        }
+
+        public async Task RegisterProductAsync(ProductDTO product)
+        {
+            var model = _mapper.Map<ProductModel>(product);
+
+            await _repository.RegisterProductAsync(model);
+        }
+    }
+}
