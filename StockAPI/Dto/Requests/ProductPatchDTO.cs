915c9e0b1c982dabac9d81354315529ceff75e99
Felipe Osorio <felipe.osorio.dev@gmail.com>
2026-07-29 04:46:50 -0300

    feat: Criado DTO para requests de PATCH.

---
+++ StockAPI/Dto/Requests/ProductPatchDTO.cs
@@ -0,0 +1,10 @@
+﻿namespace StockAPI.Dto.Requests
+{
+    public class ProductPatchDTO
+    {
+        public string? Name { get; set; }
+        public string? EAN { get; set; }
+        public int? Amount { get; set; }
+        public DateOnly? Validate { get; set; }
+    }
+}
