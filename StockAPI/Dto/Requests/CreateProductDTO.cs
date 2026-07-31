f1a3d335278ce50fb0004f1a70afaa6099bdaa91
Felipe Osorio <felipe.osorio.dev@gmail.com>
2026-07-31 04:51:28 -0300

    feat: Criado classe DTO para requests de cadastro de produtos.

---
+++ StockAPI/Dto/Requests/CreateProductDTO.cs
@@ -0,0 +1,23 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace StockAPI.Dto.Requests
+{
+    public class CreateProductDTO
+    {
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Nome do Produto é Obrigatório.")]
+        [StringLength(50, ErrorMessage = "Nome do produto Invalido. Máximo de 50 caracteres são permitidos.")]
+        public string Name { get; set; }
+
+        [Required(AllowEmptyStrings = false, ErrorMessage = "Ean é Obrigatório")]
+        [RegularExpression("^\\d{13}$", ErrorMessage = "Ean Invalido, apenas numeros são permitidos.")]
+        public string Ean { get; set; }
+
+        [Required(ErrorMessage = "Quantidade é Obrigatória.")]
+        [Range(1, 999999, ErrorMessage = "Quantidade invalida. O produto deve conter no minimo 1 e max 999999 para estoque.")]
+        public int Amount { get; set; }
+
+        [Required(ErrorMessage = "Data de Validade do produto é Obrigatória.")]
+        [RegularExpression("^\\d{4}-\\d{2}-\\d{2}$", ErrorMessage = "Formato de data invalido.")]
+        public DateOnly Validate { get; set; }
+    }
+}
