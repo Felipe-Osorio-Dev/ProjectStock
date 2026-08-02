6c795afc2cc080ee880e9895a82eee82b9ec049c
Felipe Osorio <felipe.osorio.dev@gmail.com>
2026-08-02 17:00:36 -0300

    feat: Criado dto para response da api ao client.

---
+++ StockAPI/Dto/Responses/ProductResponseDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace StockAPI.Dto.Responses
+{
+    public class ProductResponseDTO
+    {
+        public long Id { get; set; }
+        public string Name { get; set; }
+        public string EAN { get; set; }
+        public int Amount { get; set; }
+        public DateOnly Validate { get; set; }
+    }
+}
