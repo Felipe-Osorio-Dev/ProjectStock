474041687d59579c3ac1172ef0515b6dff912a3b
Felipe Osorio <felipe.osorio.dev@gmail.com>
2026-08-04 05:00:19 -0300

    feat: Criado classe para controlar a navegação da aplicação.

---
+++ StockAPP/Service/Navigation/INavigationService.cs
@@ -0,0 +1,8 @@
+﻿namespace StockAPP.Service.Navigation
+{
+    internal interface INavigationService
+    {
+        void SetParentMDI(Form parent);
+        void NavigateTo<TForm>() where TForm : Form, new();
+    }
+}
---
+++ StockAPP/Service/Navigation/NavigationService.cs
@@ -0,0 +1,41 @@
+﻿namespace StockAPP.Service.Navigation
+{
+    internal class NavigationService : INavigationService
+    {
+        private readonly IServiceProvider _provider;
+        private Form? _mdiParent;
+
+        public NavigationService(IServiceProvider provider)
+        {
+            _provider = provider;
+        }
+
+        public void NavigateTo<TForm>() where TForm : Form, new()
+        {
+            if (_mdiParent == null)
+            {
+                throw new InvalidOperationException("MDI não esta configurado");
+            }
+
+            var existingForm = _mdiParent.MdiChildren.FirstOrDefault();
+
+            if (existingForm != null)
+            {
+                existingForm.Activate();
+                return ;
+            }
+
+            var form = new TForm();
+
+            form.MdiParent = _mdiParent;
+            form.Show();
+
+
+        }
+
+        public void SetParentMDI(Form parent)
+        {
+            _mdiParent = parent;
+        }
+    }
+}
