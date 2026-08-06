12313240a0d2bbe30a86be18223f4b085ac3ec52
Felipe Osorio <felipe.osorio.dev@gmail.com>
2026-08-06 04:46:04 -0300

    feat: Criado classe presenter para lidar com a logica e os eventos das views.

---
+++ StockAPP/Presenters/Main/MainPresenter.cs
@@ -0,0 +1,35 @@
+﻿using StockAPP.Service.Navigation;
+using StockAPP.Views.MainContainer;
+using StockAPP.Views.RegisterForm;
+
+namespace StockAPP.Presenters.Main
+{
+    public class MainPresenter
+    {
+        private readonly IMainView _view;
+        private readonly INavigationService _navigationService;
+
+        public MainPresenter(IMainView view, INavigationService navigationService)
+        {
+            _view = view;
+            _navigationService = navigationService;
+
+            _navigationService.SetParentMDI((Form) _view);
+
+            _view.ClickedRegisterProducts += OnClickedRegisterProducts;
+        }
+
+        private void OnClickedRegisterProducts(object sender, EventArgs e)
+        {
+            using(RegisterDialog dialog = new RegisterDialog())
+            {
+                if(dialog.ShowDialog() == DialogResult.Cancel)
+                {
+                    return;
+                }
+
+
+            }
+        }
+    }
+}
