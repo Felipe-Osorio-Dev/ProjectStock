31baff38a01bd369a36af0a1f3504310c9a4f77c
Felipe Osorio <felipe.osorio.dev@gmail.com>
2026-08-14 05:08:56 -0300

    build: Configuração de DI

--- StockAPP/Program.cs
+++ StockAPP/Program.cs
@@ -1,9 +1,12 @@
 using Microsoft.Extensions.DependencyInjection;
 using StockAPP.Presenters.Main;
+using StockAPP.Presenters.Stock;
 using StockAPP.Service.Api;
 using StockAPP.Service.Navigation;
+using StockAPP.Util;
 using StockAPP.Views.MainContainer;
 using StockAPP.Views.RegisterForm;
+using StockAPP.Views.Stock;
 
 namespace StockAPP
 {
@@ -34,15 +37,19 @@ namespace StockAPP
 
             //Forms
             services.AddTransient<RegisterDialog>();
+            services.AddTransient<StockForm>();
 
             //Presenters
             services.AddTransient<MainPresenter>();
+            services.AddTransient<StockPresenter>();
 
             //Interfaces
             services.AddTransient<IMainView>(sp => sp.GetRequiredService<MainContainerMDI>());
+            services.AddTransient<IStockView>(sp => sp.GetRequiredService<StockForm>());
 
             //Services
             services.AddSingleton<INavigationService, NavigationService>();
+            services.AddSingleton<IFormFactory, FormFactory>();
             services.AddHttpClient<IProductService, ProductService>()
                 .ConfigureHttpClient(client =>
                 {
